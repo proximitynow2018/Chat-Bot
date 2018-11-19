763800235394bbdde04428ccf151cbafa8f18ac0
proximitynow2018 <44660823+proximitynow2018@users.noreply.github.com>
2018-11-19 15:55:25 +1300

    finally in the menu

--- Chat Bot/Chat Bot.bat
+++ Chat Bot/Chat Bot.bat
@@ -1,7 +1,9 @@
 @echo off
 title Chat Bot
+set orgcd=%cd%
+cd settings
 :test1
-if exist Settings-Speech.dat (
+if exist speech.dat (
 goto test2
 ) else (
 echo Please run the setup.bat
@@ -9,7 +11,7 @@ pause
 goto exit
 )
 :test2
-if exist Settings-Name.dat (
+if exist name.dat (
 goto start
 ) else (
 echo Please run the setup.bat
@@ -18,6 +20,45 @@ goto exit
 )
 :start
 echo Reading Settings Folder...
+echo Reading settings/speech.dat
+for /f "delims=" %%a in (speech.dat) DO ( 
+echo Speech set to %%a
+set speechpack=%%a
+)
+echo Reading settings/name.dat
+for /f "delims=" %%a in (name.dat) DO ( 
+echo Name set to %%a
+set usersname=%%a
+)
+echo Searching for speechpack...
+cd %orgcd%/speech
+if exist %speechpack% (
+goto speechexist
+) else (
+echo Cannot find speechpack: %speechpack%. Have you imported the right speechpack? Have you maybe wrote a typo?
 pause
 goto exit
+)
+:speechexist
+echo Found speechpack checking contents...
+cd %speechpack%
+tree /f /a
+echo All ready! (hopefully)
+goto chatbot
+:chatbot
+cls
+title Chatbot v1.0
+echo Chatbot v1.0
+echo github.com/proximitynow2018/chat-bot/
+echo.
+echo 1) Start chatbot
+echo.
+echo 2) Visit GitHub
+echo.
+echo 3) Exit
+echo.
+choice /c 123
+if %errorlevel% == 1 goto start
+if %errorlevel% == 2 start "" http://www.github.com/proximitynow2018/chat-bot/
+if %errorlevel% == 3 goto exit
 :exit
--- Chat Bot/Setup.bat
+++ Chat Bot/Setup.bat
@@ -1,4 +1,5 @@
 @echo off
+cd settings
 title Chat Bot Setup
 echo Chat Bot Setup
 echo github.com/proximitynow2018/chat-bot
@@ -6,9 +7,19 @@ echo.
 echo Speech File Name (Do 'none' for default)
 set /p name=
 cls
+echo Chat Bot Setup
+echo github.com/proximitynow2018/chat-bot
+echo.
 echo Your Name
 set /p yname=
 cls
+echo Chat Bot Setup
+echo github.com/proximitynow2018/chat-bot
+echo.
 echo Applying Settings...
-echo %name%>Settings-Speech.dat
-echo %name%>Settings-Name.dat
+echo %name%>speech.dat
+echo Applied %name% to settings/speech.dat
+echo %yname%>name.dat
+echo Applied %yname% to settings/name.dat
+echo All Done!
+echo Terminating Program...
